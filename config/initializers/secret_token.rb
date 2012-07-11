cba0ea6415e6d0559a57fc0b7fc7e9233d049475
Stefan Caparros <stefancaparros@gmail.com>
2012-07-11 10:06:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Km7::Application.config.secret_token = 'daf91544f1b3ca29125c408f24c95d6fe72b1f78624e5fe63c3ed46ccfa94d9c9a6573bf9f5a20e081ed2322d4b636a4736944763e2f68abe193d9ca54a58f88'
