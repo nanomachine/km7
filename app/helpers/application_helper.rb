797e77de538b73bb121f616578b5261f887f98ee
Stefan Caparros <stefancaparros@gmail.com>
2012-07-17 10:40:11 -0400

    Tweek welcome view css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Km7"
+    base_title = "Kilometro Siete"
     if page_title.empty?
       base_title
     else
