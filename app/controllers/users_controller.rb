5d6b5c149fdcffa432b83c189ad419131317d48f
Stefan Caparros <stefancaparros@gmail.com>
2012-08-06 13:59:16 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Km7::Application.routes.draw do
 resources :users
+resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   root to: 'static_pages#home'
 
   match '/ayuda',    to: 'static_pages#help'
