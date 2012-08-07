633505f2707fcc31af4a28212ddb2bfa7b568b65
Stefan Caparros <stefancaparros@gmail.com>
2012-08-07 11:05:22 -0400

    Finsh user edit, update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,16 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Credenciales incorrectos. Trate de nuevo.'
       render 'new'
     end
   end
 
   def destroy
+    sign_out
+    redirect_to root_path
   end
 	
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
+  
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+
+  before_filter :correct_user, only: [:edit, :update]
+
+  before_filter :admin_user,  only: :destroy
+
 	def show
 		@user = User.find(params[:id])
 	end
 	
-  	def new
-  		@user = User.new
-  	end
+  def new
+  	@user = User.new
+	end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def create
     @user = User.new(params[:user])
@@ -13,13 +24,45 @@ class UsersController < ApplicationController
       flash[:success] = "Bienvenido a Km7!"
       redirect_to @user
     else
+      flash.now[:error] = 'Credenciales incorrectos.'
       render 'new'
     end
   end
 
   def destroy
-    sign_out
-    redirect_to root_path
+    User.find(params[:id]).destroy
+    flash[:success] = "Usuario borrado"
+    redirect_to users_path
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Perfil actualizado"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Favor de registrarse."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user? (@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
