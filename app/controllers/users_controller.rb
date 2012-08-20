34f8214c4f9da5833a92ba9a3d36b54ee9f04a9d
Stefan Caparros <stefancaparros@gmail.com>
2012-08-20 16:55:30 -0400

    Finish production db user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  #before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :new, :create]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :new, :create]
 
   before_filter :correct_user, only: [:edit, :update]
 
