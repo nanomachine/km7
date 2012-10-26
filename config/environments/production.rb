1f4dc70d091726d9a9f5407b09cd0f0ca92a1b4a
Stefan Caparros <stefancaparros@gmail.com>
2012-10-26 11:12:30 -0400

    Remove protect from forgery, tweek aws to eliminate need for s3.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,14 @@ Km7::Application.configure do
 
   config.assets.precompile += %w( *.css *.js )
 
-  #config.paperclip_defaults = {
-  #  :storage => :s3,
-  #  :s3_credentials => {
-  #    :bucket => ENV['AWS_BUCKET'],
-  #    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  #  }
-  #}
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
