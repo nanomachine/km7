8a096565aa318c6d4bce3d395835ff6b9f23e856
Stefan Caparros <stefancaparros@gmail.com>
2013-02-18 11:36:08 -0400

    Removed config.assets.compile true, damaged heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Km7::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   #config.assets.paths << "#{Gmaps4rails::Engine.config.root}/public/stylesheets"
   #config.assets.precompile << "gmaps4rails.css"
