a519e90bc03b13997db9cdd1a96e3a36412aface
Stefan Caparros <stefancaparros@gmail.com>
2013-04-24 11:41:31 -0400

    Fixed site navigation for root_path, it is no longer accessible when signed in, as it should be

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
         sign_in user
-        redirect_back_or user
+        redirect_to a_dashboard
     else
       flash.now[:error] = 'Username or password is incorrect. Please, try again.'
       render 'new'
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out
-    redirect_to login_path
+    redirect_to root_path
   end
 	
 end
