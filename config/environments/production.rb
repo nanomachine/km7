8cd4e2897d97d5d0805a21d6d107092c1bdea663
Stefan Caparros <stefancaparros@gmail.com>
2013-07-12 14:57:21 -0400

    Updated paperclip and cocaine gems as possible fix for heroku/aws/paperclip error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Km7::Application.configure do
   config.assets.precompile << "gmaps4rails.css"
   #config.assets.precompile += %w( *.css *.js )
 
-  config.action_mailer.default_url_options = { :host => "floating-inlet-7893.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "km7.herokuapp.com" }
 
 
   #config.paperclip_defaults = {
