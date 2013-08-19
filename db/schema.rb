cdd2fd158eaff7990754d3c4f2beb55e1298bd30
Stefan Caparros <stefancaparros@gmail.com>
2013-08-19 09:45:31 -0400

    Properly added last_name migration for users to fix Heroku (production) database inconsistencies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130815212323) do
+ActiveRecord::Schema.define(:version => 20130819133604) do
 
   create_table "comments", :force => true do |t|
     t.integer  "commentable_id",   :default => 0
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(:version => 20130815212323) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "authentication_token"
+    t.string   "last_name"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
