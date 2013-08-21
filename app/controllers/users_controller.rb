d5170ed1eef60a2da6d49871ebf7e6c20fcc65b5
Stefan Caparros <stefancaparros@gmail.com>
2013-08-21 09:22:05 -0400

    Implemented update user info functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,9 +44,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
-      sign_in @user
       redirect_to @user
     else
       redirect_to @user
