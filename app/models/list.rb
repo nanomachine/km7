5af6b54140699a1daa709954078f4e362dccca44
Stefan Caparros <stefancaparros@gmail.com>
2013-08-28 12:31:15 -0400

    Rmoved unused attributes and migrations for problems and users

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ActiveRecord::Base
-	acts_as_xlsx
+	acts_as_xlsx :columns => [:id, :name, :description, :active, :user_id, :created_at, :updated_at]
+	belongs_to :user
 	belongs_to :user
 	has_and_belongs_to_many :problems, :uniq => true
 
--- db/migrate/20130121195705_add_status_update_column_to_problems.rb
+++
@@ -1,9 +0,0 @@
-class AddStatusUpdateColumnToProblems < ActiveRecord::Migration
-  def up
-    add_column :problems, :status_update, :datetime
-  end
-
-  def down
-  	remove_column :problems, :status_update, :datetime
-  end
-end
